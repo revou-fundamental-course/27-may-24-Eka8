2da10a83e06f6d2fa7ab6f84922aac17c7f56200
eka <1997ekawahyuni@gmail.com>
2024-06-06 22:49:47 +0700

    terakhir-commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 //ini masih minggu depan pembahasannya\\
+
+function formValidation() {
+    let nameInput = document.getElementById('name-input').value;
+    if (nameInput == null || nameInput == ''){
+        alert('Belum Diisi');
+    } else {
+    console.log(nameInput);
+    }
+}
+
+let indexSlide = 1;
+showBanner(1);
+
+function nextSlide(n) {
+    showBanner(indexSlide += n);
+}
+function showBanner() {
+    let listImage = document.getElementsByClassName('banner-img');
+    if (indexBanner > listImage.length) indexSlide = 1;
+
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].style.display = 'none';
+        index++;
+    }
+
+    listImage[indexSlide - 1].style.display = 'block';
+    //document.getElementsByClassName('banner-img')[0].style.display='flex';\\
+}
+
